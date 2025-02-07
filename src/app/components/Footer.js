7889c8448745e60cabcb760d338538a846f1b620
3litepwn <3litepwn@gmail.com>
2025-02-07 11:58:11 +0300

    added footer component

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+     <h2 className='font-bold text-emeral-900'> this is the footer </h2>
+    </div>
+  )
+}
+
+export default Footer

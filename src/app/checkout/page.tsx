ec028bb4af15a225dfa5d82f85cc86964369c6ac
3litepwn <3litepwn@gmail.com>
2025-02-07 10:43:49 +0300

    edited the checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <div>
+      <h1 className='font-semibold text-emerald-900'> Testing github accout </h1>
+    </div>
+  )
+}
+
+export default Checkout
